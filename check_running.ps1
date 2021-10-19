3179e156d6fb010757572501ec52b1d5d7e76c10
cgao <chengxi.gao@gmail.com>
2021-10-18 18:26:49 -0700

    added app and app_spark

--- check_running.ps1
+++ check_running.ps1
@@ -1,4 +1,4 @@
-﻿$SCR="main.py"
+﻿$SCR="app.py"
 $proc = $null
 $procs = Get-WmiObject Win32_Process -Filter "name = 'python3.exe' or name = 'python.exe'" | Select-Object Description,ProcessId,CommandLine,CreationDate
 
@@ -13,5 +13,5 @@ $procs | ForEach-Object {
 if ( $null -ne $proc ) {
     Write-Host "Process already running: $proc"
 } else {
-    py ./main.py
+    py ./app.py
 }
