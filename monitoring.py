057a022daa654d74a5b2f2066e46e2c910c4b8e2
cgao <chengxi.gao@gmail.com>
2021-10-19 18:59:33 -0700

    back to original

--- monitoring.py
+++ monitoring.py
@@ -4,31 +4,14 @@ import requests
 from webscreen import screenshot
 from emailing import SendEmail
 import datetime
-import random
-from fake_useragent import UserAgent
-import http.client
+
 
 def request(web):
-    http.client._MAXLINE = 1000000
-    ua = UserAgent()
-    headers = {'User-Agent': ua.random}
-    # headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 \
-    #                 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1'}     #设置headers信息，模拟成浏览器取访问网站
-
-    user_agent_list = [
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 10.0; …) Gecko/20100101 Firefox/61.0",
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.186 Safari/537.36",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36",
-        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36",
-        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
-        "Mozilla/5.0 (Macintosh; U; PPC Mac OS X 10.5; en-US; rv:1.9.2.15) Gecko/20110303 Firefox/3.6.15",
-    ]
-    headers['User-Agent'] = random.choice(user_agent_list)
-
-    req = requests.get(web, headers=headers, allow_redirects=False,timeout=None)   #向网站发起请求，并获取响应对象
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 \
+                    (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1'}     #设置headers信息，模拟成浏览器取访问网站
+    req = requests.get(web, headers=headers,allow_redirects=False)   #向网站发起请求，并获取响应对象
     content = req.text   #获取网站源码
+
     #'href="/files/(.*?)</a>'
     # https://www.cms.gov/Medicare/Coding/HCPCSReleaseCodeSets/HCPCS-Quarterly-Update \
     # https://www.cms.gov/medicare/icd-10/2022-icd-10-cm \
